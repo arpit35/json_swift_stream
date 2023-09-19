9258e9dcfaa59757ed7d37f3df5c94b444ca8557
arpit <arpitpandey351999@gmail.com>
2023-09-19 17:53:50 +0530

    redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
-const App = () => <div>hellow world</div>;
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<h1>ola amigo</h1>} />
+  </Routes>
+);
 
 export default App;
